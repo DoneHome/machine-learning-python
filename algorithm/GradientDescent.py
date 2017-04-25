c562e2f152133f0ef9719a96d9f38997202bdda1
DoneHome <donehomer@gmail.com>
2017-04-25 11:16:34 +0800

    add algo

---
+++ algorithm/GradientDescent.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+
+
+def BGD():
+    """
+    Batch Gradient Descent
+    """
+    pass
+
+def SGD():
+    """
+    Stochastic Gradient Descent
+    """
+    pass
+
+def MBGD():
+    """
+    Mini-batch Gradient Descent
+    """
+    pass
+
