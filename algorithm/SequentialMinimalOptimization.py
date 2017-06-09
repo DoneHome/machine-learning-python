81556666e053c6170ebd36ae5d55c25d191e3bcd
DoneHome <donehomer@gmail.com>
2017-06-09 14:43:44 +0800

    finish linear svm

--- algorithm/SequentialMinimalOptimization.py
+++ algorithm/SequentialMinimalOptimization.py
@@ -5,55 +5,67 @@ import numpy as np
 
 class SMO():
     """
+    Reference:
+        《统计学习方法》
+        《Sequential Minimal Optimization: A Fast Algorithm for Training Support Vector Machines》
+         http://www.wengweitao.com/zhi-chi-xiang-liang-ji-smoxu-lie-zui-xiao-zui-you-hua-suan-fa.html
+         http://www.cnblogs.com/jerrylead/archive/2011/03/18/1988419.html
     """
 
-    def __init__(self, alpha, bias, kernel_matrix, C, num_train, num_iters, eps, Y):
+    def __init__(self, alpha, bias, kernel_matrix, C, num_train, num_iters, tol, Y):
         """
         """
         self.alpha = alpha
         self.bias = bias
         self.kernel_matrix = kernel_matrix
         self.C = C
-        self.eps = eps
-        self.E_val = []
+        self.tol = tol
+        self.E_val = np.zeros((num_train))
         self.num_train = num_train
         self.num_iters = num_iters
         self.Y = Y
 
-    def evaluate_decision_error(self):
+    def calc_decision_error(self, i):
         """
-        Ei = g(xi) - yi
+        error = g(xi) - yi
         """
-        for i in range(self.num_train):
-            error = 0.0
-            for j in range(self.num_train):
-                error += self.alpha[j] * self.Y[j] * self.kernel_matrix[j][i]
-            error = error + self.bias - self.Y[i]
-            self.E_val.append(error)
+        g_xi= ((self.alpha * self.Y).T).dot(self.kernel_matrix[:,i]) + self.bias
+        error = float(g_xi - self.Y[i])
+        return error
 
-    def is_satisfy_KKT(self, idx):
+    def is_violate_KKT(self, i, E_i):
         """
         1) alpha == 0, yi * g(xi) >= 1
         2) 0 < alpha < C, yi * g(xi) == 1
         3) alpha = C, yi * g(xi) <= 1
         """
-        if (self.Y[idx] * self.E_val[idx] < -self.eps and self.alpha[idx] < self.C):
-            return False
-        elif (self.Y[idx] * self.E_val[idx] > self.eps and self.alpha[idx] > 0):
-            return False
-        else:
+        if (self.Y[i] * E_i < -self.tol and self.alpha[i] < self.C):
             return True
+        elif (self.Y[i] * E_i > self.tol and self.alpha[i] > 0):
+            return True
+        else:
+            return False
 
     def select_alpha_J(self, i):
         """
         """
-        Ei = self.E_val[i]
-        if Ei >=0:
-            Ej = min(self.E_val)
-        else:
-            Ej = max(self.E_val)
+        E_i = self.E_val[i]
 
-        j = self.E_val.index(Ej)
+        max_diff = 0
+        j = -1
+        E_j = 0
+
+        for k in range(self.num_train):
+            if k == i:
+                continue
+            E_k = self.calc_decision_error(k)
+            diff = abs(E_i - E_k)
+            if diff > max_diff:
+                max_diff = diff
+                j = k
+                E_j = E_k
+
+        self.E_val[j] = E_k
         return j
 
     def select_pair_alphas(self, candidateIdx):
@@ -67,10 +79,9 @@ class SMO():
                 return [i, j]
         return None
 
-    def calc_alpha_bound(self, alphas_list):
+    def calc_alpha_bound(self, i, j):
         """
         """
-        i, j = alphas_list
         alpha_i = self.alpha[i]
         alpha_j = self.alpha[j]
 
@@ -82,26 +93,33 @@ class SMO():
             H = min(self.C, self.C + alpha_j - alpha_i)
         return L, H
 
-    def calc_eta(self, alphas_list):
+    def calc_eta(self, i, j):
         """
         """
-        i, j = alphas_list
         eta = self.kernel_matrix[i][i] + self.kernel_matrix[j][j] - 2*self.kernel_matrix[i][j]
         return eta
 
-    def update_alpha_i(self, alphas_list, alpha_j_new):
+    def update_alpha_i(self, i, j, alpha_j_new):
         """
         """
-        i, j = alphas_list
         alpha_i_new = self.alpha[i] + self.Y[i]*self.Y[j]*(self.alpha[j] - alpha_j_new)
         return alpha_i_new
 
-    def update_alpha_j(self, alphas_list, eta):
+    def update_alpha_j(self, i, j , eta):
         """
         """
-        i, j = alphas_list
         alpha_j_new = self.alpha[j] + self.Y[j]*(self.E_val[i] - self.E_val[j])*1.0/eta
         return alpha_j_new
+
+    def update_E_val(self, i, j):
+        """
+        """
+        E_i = self.calc_decision_error(i)
+        E_j = self.calc_decision_error(j)
+
+        self.E_val[i] = E_i
+        self.E_val[j] = E_j
+        return
 
     def clip_alpha(self, alpha, L, H):
         """
@@ -112,11 +130,9 @@ class SMO():
             alpha = L
         return alpha
 
-    def update_bias(self, alphas_list, alpha_i_new, alpha_j_new):
+    def update_bias(self, i, j, alpha_i_new, alpha_j_new):
         """
         """
-        i, j = alphas_list
-
         b1_new = -self.E_val[i] - self.Y[i]*self.kernel_matrix[i][i]*(alpha_i_new - self.alpha[i]) - self.Y[j]*self.kernel_matrix[j][i]*(alpha_j_new - self.alpha[j]) + self.bias
         b2_new = -self.E_val[j] - self.Y[i]*self.kernel_matrix[i][j]*(alpha_i_new - self.alpha[i]) - self.Y[j]*self.kernel_matrix[j][j]*(alpha_j_new - self.alpha[j]) + self.bias
 
@@ -128,63 +144,97 @@ class SMO():
             b_new = (b1_new + b2_new)*1.0/2
         return b_new
 
-    def solve(self, candidateIdx):
+    def takeStep(self, i, j):
         """
-        step 1: select alpha_i and alpha_j
-        step 2: calculate L and H
-        step 3: calculate eta
-        step 4: update alpha_j
-        step 5: clip alpha_j
-        step 6: update alpha_i
-        step 7: check the effectiveness of alpha_j
-        step 8: update bias
-        step 9: update E_val
-        ...
         """
-        # step 1
-        alphas_list = self.select_pair_alphas(candidateIdx)
-        if alphas_list:
-            pass
-        # step 2
-        L, H = self.calc_alpha_bound(alphas_list)
-        # step 3
-        eta = self.calc_eta(alphas_list)
-        # step 4
-        alpha_j_new = self.update_alpha_j(alphas_list, eta)
-        # step 5
-        alpha_j_clip = self.clip_alpha(alpha_j_new, L, H)
-        # step 6
-        alpha_i_new = self.update_alpha_i(alphas_list, alpha_j_new)
-        # step 7
-        pass
-        # step 8
-        bias_new = self.update_bias(alphas_list, alpha_i_new, alpha_j_new)
-        # step 9
-        pass
+        if i == j:
+            return 0
+
+        # step 1: calculate L and H
+        L, H = self.calc_alpha_bound(i, j)
+        if L == H:
+            return 0
+        # step 2: calculate eta
+        eta = self.calc_eta(i, j)
+        if eta <= 0:
+            return 0
+        # step 3: update alpha_j
+        alpha_j_new = self.update_alpha_j(i, j, eta)
+        # step 4: clip alpha_j
+        alpha_j_new = self.clip_alpha(alpha_j_new, L, H)
+        # step 5: check the effectiveness of alpha_j
+        if abs(alpha_j_new - self.alpha[j]) < 0.0001:
+            return 0
+        # step 6: update alpha_i
+        alpha_i_new = self.update_alpha_i(i, j, alpha_j_new)
+        # step 7: update bias
+        self.bias = self.update_bias(i, j, alpha_i_new, alpha_j_new)
+        # step 8: store new alpha_i, alpha_j
+        self.alpha[i] = alpha_i_new
+        self.alpha[j] = alpha_j_new
+        # step 9: update E_val
+        self.update_E_val(i, j)
+
+        return 1
+
+    def examineExample(self, i):
+        """
+        """
+        E_i = self.calc_decision_error(i)
+        self.E_val[i] = E_i
+        #print "%s\t%s" %(i, E_i)
+
+        if self.is_violate_KKT(i, E_i):
+            #print "viloate KKT:%s" %i
+            nonBoundJdx = np.nonzero((self.alpha > 0) * (self.alpha < self.C))[0]
+            #
+            if len(nonBoundJdx) > 1:
+                #print "enter loop 1"
+                j = self.select_alpha_J(i)
+                if self.takeStep(i, j):
+                    return 1
+
+            # loop over all non-zero and non-C alpha, starting at a random point
+            for j in nonBoundJdx:
+                #print "enter loop 2"
+                if self.takeStep(i, j):
+                    return 1
+
+            # loop over all possible i1, starting at a random point
+            for j in range(self.num_train):
+                #print "enter loop 3"
+                if self.takeStep(i, j):
+                    return 1
+        return 0
 
     def optimize(self):
         """
         """
         _iter = 0
-        self.evaluate_decision_error()
 
-        entireSet = True
+        examineAll = True
+        numChanged = 0
 
-        while _iter < self.num_iters:
-            if entireSet:
+        while _iter < self.num_iters and (numChanged > 0 or examineAll):
+            numChanged = 0
+
+            if examineAll:
                 # update alpha through all training samples
-                candidateIdx = np.array(range(self.num_train))
-                flag = self.solve(candidateIdx)
+                for i in range(self.num_train):
+                    numChanged += self.examineExample(i)
+                #print "iter:%s\texamineAll\tnumChanged:%s" %(_iter, numChanged)
             else:
                 # update alpha through samples that 0 < alpha_i < C
-                candidateIdx = np.nonzero((self.alpha > 0) * (self.alpha < self.C))[0]
-                flag = self.solve(candidateIdx)
+                nonBoundIdx = np.nonzero((self.alpha > 0) * (self.alpha < self.C))[0]
+                for i in nonBoundIdx:
+                    numChanged += self.examineExample(i)
+                #print "iter:%s\tnumChanged\tnumChanged:%s" %(_iter, numChanged)
 
-            if entireSet:
-                entireSet = False
-            elif flag == False:
-                entireSet = True
+            if examineAll:
+                examineAll = False
+            elif numChanged == 0:
+                examineAll = True
 
             _iter += 1
-            return
+        return self.alpha, self.bias
 
--- algorithm/SupportVectorMachine.py
+++ algorithm/SupportVectorMachine.py
@@ -4,7 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from SequentialMinimalOptimization import SMO
 
-data = np.genfromtxt("./data/gaussian_discriminant_analysis.csv", dtype=np.float64, delimiter=",")
+#data = np.genfromtxt("./data/gaussian_discriminant_analysis.csv", dtype=np.float64, delimiter=",")
+data = np.genfromtxt("./data/svm.csv", dtype=np.float64, delimiter=",")
 fig = plt.figure()
 
 def plot_estimator(estimator, X, y):
@@ -33,6 +34,37 @@ def plot_estimator(estimator, X, y):
     pl.axis('off')
     pl.tight_layout()
 
+def draw(data, sv, w, b):
+    """
+    """
+    pos_data = data[data[:,-1]==1]
+    neg_data = data[data[:,-1]==-1]
+    plt.scatter(pos_data[:,0], pos_data[:,1], color="red")
+    plt.scatter(neg_data[:,0], neg_data[:,1], color="blue")
+
+    plt.scatter(sv[:,0], sv[:,1], color="green")
+
+    min_x = min(data[:, 0])
+    max_x = max(data[:, 0])
+
+
+    min_y = float(-b - w[0] * min_x) / w[1]
+    max_y = float(-b - w[0] * max_x) / w[1]
+    plt.plot([min_x, max_x], [min_y, max_y], '-')
+
+    #min_y_down = float(-b - w[0] * min_x - r) / w[1]
+    #max_y_down = float(-b - w[0] * max_x - r) / w[1]
+    #plt.plot([min_x, max_x], [min_y_down, max_y_down], '--')
+
+    #min_y_up = float(-b - w[0] * min_x + r) / w[1]
+    #max_y_up = float(-b - w[0] * max_x + r) / w[1]
+    #plt.plot([min_x, max_x], [min_y_up, max_y_up], '--')
+
+    plt.xlabel('x1')
+    plt.ylabel('x2')
+
+    plt.show()
+
 class SVM():
     """
     Support Vector Machine
@@ -52,10 +84,12 @@ class SVM():
         self.kernel_matrix = None
         self.C = C
         self.zeta = 0
-        self.eps = 0.001
+        self.tol= 0.001
 
         self.num_train = None
         self.num_iters = num_iters
+
+        self.support_vectors = None
 
     def hypothesis_func(self, x):
         """ h = sign(w.T * x + bias)
@@ -73,7 +107,7 @@ class SVM():
         elif self.kernel == "polynomial":
             pass
         elif self.kernel == "None":
-            pass
+            return x.dot(z.T)
         else:
             raise Exception("The kernel type of %s is not supported" % opt)
 
@@ -85,7 +119,7 @@ class SVM():
 
         for i in range(self.num_train):
             x_i = X[i]
-            for j in range(i+1, self.num_train):
+            for j in range(i, self.num_train):
                 x_j = X[j]
                 self.kernel_matrix[i, j] = self.evaluate_kernel_value(x_i, x_j)
                 self.kernel_matrix[j, i] = self.kernel_matrix[i, j]
@@ -97,23 +131,23 @@ class SVM():
         self.alpha = np.zeros((self.num_train, 1))
         self.gen_kernel_matrix(X)
 
-        smo = SMO(self.alpha, self.bias, self.kernel_matrix, self.C, self.num_train, self.num_iters, self.eps, Y)
-        smo.optimize()
+        smo = SMO(self.alpha, self.bias, self.kernel_matrix, self.C, self.num_train, self.num_iters, self.tol, Y)
+        self.alpha, self.bias = smo.optimize()
+
+        sv_idx = np.nonzero((self.alpha > 0) * (self.alpha < self.C))[0]
+        self.support_vectors = X[sv_idx]
+
 
 if __name__ == "__main__":
     trainX = data[:,:2]
     trainY = data[:,-1:]
-    trainY[trainY==0] = -1
+    #trainY[trainY==0] = -1
 
-    clf = SVM(C=1, kernel="rbf", num_iters=100, gamma=1.0)
+    clf = SVM(C=100, kernel="None", num_iters=1000, gamma=1.0)
     clf.train(trainX, trainY)
+    sv = clf.support_vectors
 
-    pos_data = data[data[:,-1]==1]
-    neg_data = data[data[:,-1]==0]
-    plt.scatter(pos_data[:,0], pos_data[:,1], color="red")
-    plt.scatter(neg_data[:,0], neg_data[:,1], color="blue")
+    w = (clf.alpha * trainY).T.dot(trainX)[0]
 
-    # 绘制支持向量
-    #pl.scatter(svc.support_vectors_[:, 0], svc.support_vectors_[:, 1], s=80, facecolors='none', zorder=10)
-    #plt.show()
+    draw(data, sv, w, clf.bias)
 
